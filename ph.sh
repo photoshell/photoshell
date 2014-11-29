65d3eb304c132157a0ef3885efb01c2fbcde9687
Cameron Paul <cpaul37@gmail.com>
2014-11-28 21:01:03 -0600

    Added import option

--- ph.sh
+++ ph.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo "ph.sh"
+
+while getopts ":i:" opt; do
+    case $opt in
+        i)
+            echo "Importing $OPTARG"
+            ;;
+        \?)
+            echo "ERROR: -$OPTARG is an invalid option." >&2
+            ;;
+        :)
+            echo "ERROR: -$OPTARG takes an argument." >&2
+        esac
+done
