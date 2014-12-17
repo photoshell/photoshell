848d200f999d2759a7cca1ea4506a27b02ae66a9
Cameron Paul <cpaul37@gmail.com>
2014-12-16 20:49:18 -0600

    Setup python project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='photoshell',
+    version='0.0.0',
+    description='Utilities for Managing Photos',
+    author='Cameron Paul',
+    author_email='cpaul37@gmail.com',
+    url='https://github.com/campaul/ph.sh',
+    packages=['photoshell'],
+)
