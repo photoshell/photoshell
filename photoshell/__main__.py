2d15ff38abb68335daa8bb2b94aaeff91ed829a2
Sam Whited <sam@samwhited.com>
2014-12-21 17:46:45 -0500

    Create default config if one doesn't exist
    Fixes #20

--- photoshell/__main__.py
+++ photoshell/__main__.py
@@ -5,11 +5,19 @@ import yaml
 
 from photoshell import ui
 
-
 config_path = os.path.join(os.environ['HOME'], '.photoshell.yaml')
 
-with open(config_path, 'r') as config_file:
-    config = yaml.load(config_file)
+config = dict(
+    {
+        'library': os.path.join(os.environ['HOME'], 'Pictures/Photoshell')
+    }
+)
+if os.path.isfile(config_path):
+    with open(config_path, 'r') as config_file:
+        config = yaml.load(config_file)
+else:
+    with open(config_path, 'w+') as config_file:
+        yaml.dump(config, config_file, default_flow_style=False)
 
 print('Libray path is {0}'.format(config['library']))
 
