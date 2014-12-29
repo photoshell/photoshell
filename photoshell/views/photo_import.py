37b4183da6eb65ca82dc5794472757aab05ff584
Cameron Paul <cpaul37@gmail.com>
2014-12-28 23:54:06 -0600

    Don't re-render during import (results in at least 10x speedup)

--- photoshell/views/photo_import.py
+++ photoshell/views/photo_import.py
@@ -1,5 +1,6 @@
 import threading
 
+from gi.repository import GLib
 from gi.repository import Gtk
 
 
@@ -30,25 +31,27 @@ class PhotoImporter(Gtk.FileChooserDialog):
 
         if filename:
             def do_import():
-                self._window.import_button.set_sensitive(False)
-                self._window.progress.set_fraction(0)
-                self._window.header_bar.pack_start(self._window.progress)
-                self._window.show_all()
+                GLib.idle_add(self._window.import_button.set_sensitive, False)
+                GLib.idle_add(self._window.progress.set_fraction, 0)
+                GLib.idle_add(
+                    self._window.header_bar.pack_start, self._window.progress)
+                GLib.idle_add(self._window.show_all)
 
                 def imported(photo_hash, percent):
-                    self._window.progress.set_fraction(percent)
-                    # TODO: this is kind of awkward
-                    self._window.render_selection(self._window.library.update(
-                        self._window.selection))
+                    GLib.idle_add(self._window.progress.set_fraction, percent)
 
                 def notify_progress(photo_name):
-                    self._window.progress.set_text(photo_name)
+                    GLib.idle_add(self._window.progress.set_text, photo_name)
 
                 self._window.library.import_photos(
                     filename, notify=notify_progress, imported=imported)
 
-                self._window.import_button.set_sensitive(True)
-                self._window.header_bar.remove(self._window.progress)
+                GLib.idle_add(self._window.import_button.set_sensitive, True)
+                GLib.idle_add(
+                    self._window.header_bar.remove, self._window.progress)
+                GLib.idle_add(self._window.render_selection, self._window.library.update(
+                    self._window.selection
+                ))
 
             thread = threading.Thread(target=do_import)
             thread.daemon = True
