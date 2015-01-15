1e959cc19cd900e885001fd13233ed6b693041de
Cameron Paul <cpaul37@gmail.com>
2015-01-14 21:50:39 -0600

    Added a couple simple tests, setup coverage

--- photoshell/library.py
+++ photoshell/library.py
@@ -1,9 +1,9 @@
 import os
 
-from photoshell.image import Image
 from photoshell.photo import Photo
 
 from photoshell import raw
+from photoshell.selection import Selection
 from photoshell.util import hash_file
 from photoshell.util import Progress
 
@@ -39,9 +39,8 @@ class Library(object):
     def query(self, match):
         selection = Selection(self.library_path, match)
         for sidecar in self.sidecars:
-            image = Image(sidecar.developed_path, sidecar.datetime)
-            if match(image):
-                selection.append(image)
+            if match(sidecar):
+                selection.append(sidecar)
 
         # TODO: in place mutation is terrible
         selection.sort(key=lambda image: image.datetime)
@@ -57,10 +56,10 @@ class Library(object):
         return new_selection
 
     def exists(self, photo):
-        return not filter(
-            lambda s: s.file_hash == photo.file_hash,
-            self.sidecars
-        )
+        for sidecar in self.sidecars:
+            if photo.file_hash == sidecar.file_hash:
+                return True
+        return False
 
     def add(self, photo):
         self.sidecars.append(photo)
@@ -126,51 +125,3 @@ class Library(object):
                 photo_list.append(photo)
 
         return photo_list
-
-
-class Selection(object):
-
-    def __init__(self, library_path, query):
-        super(Selection, self).__init__()
-
-        self.library_path = library_path
-        self.query = query
-        self.images = []
-        self.current_image = 0
-
-    def append(self, image):
-        self.images.append(image)
-
-    def current(self):
-        if len(self.images):
-            return self.images[self.current_image]
-        else:
-            return None
-
-    def next(self):
-        l = len(self.images)
-        if l > 1:
-            self.current_image = (self.current_image + 1) % l
-        return self.current()
-
-    def prev(self):
-        l = len(self.images)
-        if l > 1:
-            self.current_image = (self.current_image - 1) % l
-        return self.current()
-
-    def jump(self, image_path):
-        # TODO: there is an idiomatic way to do this
-        for i in range(len(self.images)):
-            if self.images[i].image_path == image_path:
-                self.current_image = i
-                break
-
-        return self.current()
-
-    def each(self):
-        for image in self.images:
-            yield image
-
-    def sort(self, key):
-        self.images = sorted(self.images, key=key)
