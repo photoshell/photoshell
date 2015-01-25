3180bca6305ea470f25282349db6233b4144a0d0
Sam Whited <sam@samwhited.com>
2015-01-24 20:32:34 -0500

    Finish testing selection

--- tests/selection_test.py
+++ tests/selection_test.py
@@ -12,41 +12,52 @@ def sidecar():
 
 
 @pytest.fixture
-def empty_selection():
+def selection():
     s = Selection('', '')
     return s
 
 
-@pytest.fixture
-def selection(empty_selection, sidecar):
-    empty_selection.append(sidecar)
-    return empty_selection
+def test_append(selection, sidecar):
+    selection.append(sidecar)
+    assert selection.photos[0] == sidecar
 
 
-def test_append(empty_selection, sidecar):
-    empty_selection.append(sidecar)
-    assert empty_selection.photos[0] == sidecar
-
-
-def test_current_photo_default_selection(selection):
+def test_current_photo_default_selection(selection, sidecar):
+    selection.append(sidecar)
     assert selection.current_photo()
 
 
-def test_current_photo_is_none_if_selection_empty(empty_selection):
-    assert empty_selection.current_photo() is None
+def test_current_photo_is_none_if_selection_empty(selection):
+    assert selection.current_photo() is None
 
 
-def test_next_prev_does_nothing_single_photo(selection):
+def test_next_prev_does_nothing_single_photo(selection, sidecar):
+    selection.append(sidecar)
     assert selection.current_photo() == selection.next_photo()
     assert selection.current_photo() == selection.prev_photo()
 
 
-def test_next_prev_wrap_around(empty_selection, sidecar):
+def test_next_prev_wrap_around(selection, sidecar):
     sidecar2 = {'datetime': 'datetime'}
-    empty_selection.photos.append(sidecar)
-    empty_selection.photos.append(sidecar2)
+    selection.photos.append(sidecar)
+    selection.photos.append(sidecar2)
 
-    assert empty_selection.next_photo() == sidecar2
-    assert empty_selection.next_photo() == sidecar
-    assert empty_selection.prev_photo() == sidecar2
-    assert empty_selection.prev_photo() == sidecar
+    assert selection.next_photo() == sidecar2
+    assert selection.next_photo() == sidecar
+    assert selection.prev_photo() == sidecar2
+    assert selection.prev_photo() == sidecar
+
+
+def test_photo_generator(selection, sidecar):
+    sidecar2 = "Pretend I'm a sidecar"
+    selection.append(sidecar)
+    selection.append(sidecar2)
+    assert list(selection.each_photo()) == [sidecar, sidecar2]
+
+
+def test_sort(selection, sidecar):
+    sidecar2 = {'datetime': "0"}
+    selection.append(sidecar)
+    selection.append(sidecar2)
+    selection.sort(key=lambda selection: selection['datetime'])
+    assert list(selection.each_photo()) == [sidecar2, sidecar]
